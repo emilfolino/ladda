003b6b0e1dff822250e5f567bfbdbc5f90d3a5a9
Emil Folino <emilfolino@gmail.com>
2022-03-29 09:22:04 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+const posts = require("./routes/posts.js");
+
+const port = 8345;
+
+app.use(cors());
+app.options('*', cors());
+
+app.disable('x-powered-by');
+
+app.set("view engine", "ejs");
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/posts", posts);
+
+app.get('/', (req, res) => {
+    res.redirect('/documentation.html');
+});
+
+const server = app.listen(port, () => {
+    console.log('auth api listening on port ' + port);
+});
+
+module.exports = server;
